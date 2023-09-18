2aa44b84dca074db4198704965ce28d9acf1025c
Frier03 <peter.frier12345@gmail.com>
2023-09-18 17:35:07 +0200

    Update accounts.js

--- cli/components/accounts.js
+++ cli/components/accounts.js
@@ -1,17 +1,19 @@
 const inquirer = require('inquirer');
 const { getAccounts } = require('../../utils/csv')
+const { showAccounts } = require('./show_accounts');
+
 const accounts = async (backFunction) => {
     console.clear();
 
     // Get number of accounts configured
-    const accounts = await getAccounts();
+    const accountsList = await getAccounts();
 
     const { action } = await inquirer.prompt({
         type: 'list',
         name: 'action',
         message: 'Nebula Client | Accounts',
         choices: [
-            `Accounts(${accounts.length})`,
+            `Show Accounts(${accountsList.length})`,
             'Add Account',
 
             new inquirer.Separator(), // Add a separator line
@@ -20,6 +22,9 @@ const accounts = async (backFunction) => {
     });
 
     switch (action) {
+        case `Show Accounts(${accountsList.length})`:
+            showAccounts(backFunction, accountsList)
+            break;
         case 'Back':
             backFunction();
             break;
@@ -27,5 +32,5 @@ const accounts = async (backFunction) => {
 };
 
 module.exports = {
-    showAccounts: accounts,
+    showAccountsMenu: accounts,
 };
