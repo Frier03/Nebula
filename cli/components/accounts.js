87c3a9435e9dd8381b57d8c1eb7e451255e6249e
Frier03 <peter.frier12345@gmail.com>
2023-09-18 16:57:35 +0200

    Accounts now show how many account has been added

--- cli/components/accounts.js
+++ cli/components/accounts.js
@@ -1,12 +1,17 @@
 const inquirer = require('inquirer');
-
+const { getAccounts } = require('../../utils/csv')
 const accounts = async (backFunction) => {
     console.clear();
+
+    // Get number of accounts configured
+    const accounts = await getAccounts();
+
     const { action } = await inquirer.prompt({
         type: 'list',
         name: 'action',
         message: 'Nebula Client | Accounts',
         choices: [
+            `Accounts(${accounts.length})`,
             'Add Account',
 
             new inquirer.Separator(), // Add a separator line
