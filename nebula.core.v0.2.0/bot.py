09a984ef7639a8246bc573ba15941a21ecab1dfd
Frier03 <peter.frier12345@gmail.com>
2023-10-09 09:55:38 +0200

    Update `connect()`with events

--- nebula.core.v0.2.0/bot.py
+++ nebula.core.v0.2.0/bot.py
@@ -1,29 +1,61 @@
 # nebula.core.v0.2.0.bot.py
+import threading
 from enum import Enum
 import time
 
-class MinecraftBot():
+from javascript import require, On
+mineflayer = require('mineflayer')
+
+class MinecraftBot(threading.Thread):
     class State(Enum): 
         idle = 0
         connecting = 1
         connected = 2
+        failed_to_connect = 3
 
     def __init__(self) -> None:
+        super().__init__()
         self.id = id(self)
         self.state = MinecraftBot.State.idle
+        self.event = threading.Event()
+        self.entity = None
+
+    def run(self):
+        while True:
+            print(f"Bot {self.id} - {self.state.name}")
+            time.sleep(5)
 
     def connect(self, credentials):
-        """Simulate a connection process"""
-        print(f"Bot {self.id} is connecting...")
+        """Connection process"""
+        #print(f"Bot {self.id} is connecting...")
 
         # Set state "connecting" until the bot has connected
         self.state = MinecraftBot.State.connecting
+
+        self.entity = mineflayer.createBot({ 
+            'host': 'localhost',
+            'username': credentials.get('email'),
+            'password': credentials.get('password'),
+            'auth': credentials.get('auth'),
+            'hideErrors': True
+        })
         
-        # Simulate a connection process asynchronously
-        time.sleep(2)
+        @On(self.entity, 'error')
+        def onError(this, err):
+            if err.details == None: return
 
-        # Update the state to "connected"
-        self.state = MinecraftBot.State.connected
-        print(f"Bot {self.id} is connected. State: {self.state.name}")
+            self.state = MinecraftBot.State.failed_to_connect
+            #print(err.details.reason)
 
-        return MinecraftBot.State.connected
+        @On(self.entity, 'spawn')
+        def onSpawn(this):
+            self.state = MinecraftBot.State.connected
+            self.start()
+        
+        while self.state == MinecraftBot.State.connecting:
+            # No redundant check is needed
+            # Await an event to be triggered to set the bot state
+            pass
+
+        #print(f"Bot {self.id}. State: {self.state.name}")
+        return self.state
