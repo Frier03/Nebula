28f3e4b01920b1b2a69d76a10c07b17fa34c9661
Frier03 <peter.frier12345@gmail.com>
2023-10-09 18:29:11 +0200

    Update prints

--- nebula.core.v0.2.0/bot.py
+++ nebula.core.v0.2.0/bot.py
@@ -16,28 +16,30 @@ class MinecraftBot(threading.Thread):
     def __init__(self) -> None:
         super().__init__()
         self.id = id(self)
+        self.entity = None
         self.state = MinecraftBot.State.idle
         self.event = threading.Event()
-        self.entity = None
 
     def run(self):
         while True:
             print(f"Bot {self.id} - {self.state.name}")
             time.sleep(5)
 
-    def connect(self, credentials):
+    def connect(self, credentials, serverAddress):
         """Connection process"""
-        #print(f"Bot {self.id} is connecting...")
+        print(f"Bot {self.id} is connecting...")
 
         # Set state "connecting" until the bot has connected
         self.state = MinecraftBot.State.connecting
 
         self.entity = mineflayer.createBot({ 
-            'host': 'localhost',
+            'host': serverAddress[0],
+            'port': serverAddress[1],
             'username': credentials.get('email'),
             'password': credentials.get('password'),
             'auth': credentials.get('auth'),
-            'hideErrors': True
+            'hideErrors': True,
+            'logErrors': False
         })
         
         @On(self.entity, 'error')
@@ -45,11 +47,13 @@ class MinecraftBot(threading.Thread):
             if err.details == None: return
 
             self.state = MinecraftBot.State.failed_to_connect
-            #print(err.details.reason)
+            print(err.details.reason)
 
         @On(self.entity, 'spawn')
         def onSpawn(this):
             self.state = MinecraftBot.State.connected
+            #self.imageUrl = f"https://mc-heads.net/avatar/{self.entity.username}/600.png"
+
             self.start()
         
         while self.state == MinecraftBot.State.connecting:
@@ -59,3 +63,6 @@ class MinecraftBot(threading.Thread):
 
         #print(f"Bot {self.id}. State: {self.state.name}")
         return self.state
+    
+    def __setup_events(self) -> None:
+        pass
